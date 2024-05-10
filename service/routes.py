bf9e736f9ea472aa9c0d2a68a6f829542537360b
LilShocko <acemagic@mail.bg>
2024-05-10 04:04:29 -0400

    delete-list

--- service/routes.py
+++ service/routes.py
@@ -108,7 +108,7 @@ def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
 
     account = Account.find(account_id)
-     if not account:
+    if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
@@ -118,8 +118,22 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
 
-# ... place you code here to DELETE an account ...
+    # use the Account.find() method to retrieve the account by the account_id
+    account = Account.find(account_id)
+    # if found, call the delete() method on the account
+    if account:
+        account.delete()
+    # return and empty body ("") with a return code of status.HTTP_204_NO_CONTENT
+
+    return "",status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
